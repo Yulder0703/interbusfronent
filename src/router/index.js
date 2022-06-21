c54beb18300a1eea7cff867bd138c5255edab82e
VelascoVale <vcaicedo89@misena.edu.co>
2022-06-21 09:52:07 -0500

    vista despachador conductores

--- src/router/index.js
+++ src/router/index.js
@@ -151,7 +151,17 @@ const routes = [
       
     ]
 
-  }
+  },
+
+  {
+
+    path: '/despacharConductores',
+    name: 'DespachadorView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/DashboardDespachador/DespachadorView')
+  },
  
   
 ]
